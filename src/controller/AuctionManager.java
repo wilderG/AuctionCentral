a3c2890415c6d22194d4fa42e65a6f1f9b738654
Jared Malone <jaredmm@uw.edu>
2018-04-27 12:49:59 -0700

    Added Auction Manager class and method stubs.

--- src/controller/AuctionManager.java
+++ src/controller/AuctionManager.java
@@ -1,6 +1,7 @@
 package controller;
 
 import model.Auction;
+import model.AuctionCalendar;
 import model.Bid;
 import model.Item;
 import model.NewAuctionRequest;
@@ -15,16 +16,45 @@ import model.User;
  *
  */
 public class AuctionManager implements Manager {
-
-	/** Generate Serial Version UID. */
+	
+	/** The default maximum auction items for an auction. **/
+	private static final int MAXIMUM_AUCTION_ITEMS = 10;
+	
+	/** The default number of bids a bidder may place in any one auction. **/
+	private static final int MAXIMUM_BIDDER_BIDS_PER_AUCTION = 4;
+		
+	/** The maximum number of future auctions that may be scheduled. **/
+	private static final int MAXIMUM_FUTURE_AUCTIONS = 25;
+	
+	/** The maximum number of auctions allowed on any day. **/
+	private static final int MAXIMUM_AUCTIONS_PER_DAY = 2;
+	
+	/** The maximum number of days in the future that a new auction may be scheduled. **/
+	private static final int MAXIMUM_DAYS_OUT = 60;
+	
+	/** The minimum number of days in the future that a new auction may be scheduled. **/
+	private static final int MINIMUM_DAYS_OUT = 14;
+		
+	/** The maximum number of bids a bidder may place. **/
+	private static final int MAXIMUM_BIDDER_BIDS = 10;
+		
+	/** The schedule of all past and future auctions. **/
+	private AuctionCalendar myCalendar;
+	
+	/** Generated Serial Version UID. */
 	private static final long serialVersionUID = 4646613498773772086L;
 
+	public AuctionManager() {
+		// not connected to StorageIO
+		myCalendar = new AuctionCalendar(); 
+	}
+	
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public User getUser(String theUsername) {
-		// TODO Auto-generated method stub
+		// not connected to StorageIO
 		return new User(theUsername, "Test User");
 	}
 
@@ -42,7 +72,7 @@ public class AuctionManager implements Manager {
 	 */
 	@Override
 	public User submitNewUser(NewUserRequest theUserRequest) {
-		// TODO Auto-generated method stub
+		// not connected to StorageIO
 		return new User(theUserRequest.getMyUsername(), "Test User"); 
 	}
 
