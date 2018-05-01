025d2b789b68aabc9e0a5b6b1eac25f1a7d2b24c
Steven Golob <golobs@uw.edu>
2018-05-01 07:44:58 -0700

    Added auctionCalendar and Date

--- src/model/Auction.java
+++ src/model/Auction.java
@@ -1,22 +1,29 @@
 package model;
 
 import java.io.Serializable;
+import java.util.Date;
+import java.util.LinkedList;
+import java.util.List;
 
 public class Auction implements Serializable {
 
 	/** Class Serial ID -- Changing this will corrupt the data storage! */
 	private static final long serialVersionUID = -5386584822819727993L;
 	
-	/** Test Field **/
-	private final String myName;
+	public static final int MAX_ITEM_COUNT = 10;
+	//private final Date myDate;
+	
+	//private List<AuctionItem> myInventory;
 	
 	/** Constructor takes a test string. **/
-	public Auction(String name) {
-		myName = name;
+	public Auction() {
+	    //myDate = theDate;
+	    //myInventory = new LinkedList<>();
 	}
 	
+	@Override
 	public String toString() {
-		return myName;
+	    return "";
 	}
 	
 }
