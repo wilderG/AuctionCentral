aa690731c2834e6eda98dd93daccdd6284a96929
jim-rosales <rosales.jim@outlook.com>
2018-05-02 23:47:17 -0700

    Update the NonProfitContact class to reflect updated UML

--- src/model/NonProfitContact.java
+++ src/model/NonProfitContact.java
@@ -3,12 +3,14 @@
  */
 package model;
 
+import java.time.LocalDate;
+
 /**
  * Class represents a contact person for a non profit organization.
  * @author Jim Rosales
  */
 public class NonProfitContact extends User {
-	AuctionDate myDate = new AuctionDate();
+//	AuctionDate myDate = new AuctionDate();
 
 
 	/**
@@ -19,7 +21,20 @@ public class NonProfitContact extends User {
 	}
 	
 	/**
-	 * cheking if the priorExist or not.
+	 * Determines whether the given date is one year after the non profits most recent auction.
+	 * @return True if a year has elapsed for the non profits last auction.
+	 */
+	public boolean isDateOneYearAfterPreviousAuction(LocalDate theDate) {
+		LocalDate mostRecentDate = getPriorDate();
+		
+		// Found the most recent date that the non profit had an auction. Add a year to this date
+		// Now theDate has to occur on this date of after for it to be a viable date. 
+		mostRecentDate.plusYears(1); 
+		return mostRecentDate.isEqual(theDate) || theDate.isAfter(mostRecentDate);
+	}
+	
+	/**
+	 * Checks if the priorExist or not.
 	 * @return
 	 */
 	public boolean isPriorExist() {
@@ -27,8 +42,20 @@ public class NonProfitContact extends User {
 		return false;
 	}
 	
-	public AuctionDate getPriorDate() {
-		return myDate;
+	/**
+	 * Gets the date of the non profits most recent auction that has passed.
+	 * @return A LocalDate that represents the date of the non profits most recent auction.
+	 */
+	public LocalDate getPriorDate() {
+		LocalDate mostRecentDate = LocalDate.MIN;
+		LocalDate currentDate = LocalDate.now();
+		for (Auction auction: super.getMyAuctions()) {
+			LocalDate auctionsDate = auction.getDate();
+			if (auctionsDate.isBefore(currentDate) && auctionsDate.compareTo(mostRecentDate) > 0) {
+				mostRecentDate = auctionsDate;
+			}
+		}
+		return mostRecentDate;
 	}
 	
 
