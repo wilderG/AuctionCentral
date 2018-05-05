522b3a0df30e6d9c59de15c9c963741f5a4f00b4
Steven Golob <golobs@uw.edu>
2018-05-05 11:56:22 -0700

    Fixed errors in AuctionDateTest and Itemtest. Also, Changed ItemTest name to "AuctionItemTest" to match the name of the class it is testing

--- src/model/AuctionDateTest.java
+++ src/model/AuctionDateTest.java
@@ -5,10 +5,12 @@
  */
 package model;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.time.LocalDate;
+
+import org.junit.Before;
 import org.junit.Test;
 
 
@@ -21,59 +23,81 @@ import org.junit.Test;
  * @version April 30, 2018
  */
 public class AuctionDateTest {
+    
+    private LocalDate oneMonthFromToday;
+    
+    private AuctionDate dateWithNoAuctions;
+    
+    private AuctionDate dateWithOneLessThanCapacityAuctions;
+    
+    private AuctionDate dateWithCapacityAuctions;
+    
+    @Before
+    public void setup() {
+        oneMonthFromToday = LocalDate.now().plusMonths(1);
+        dateWithNoAuctions = new AuctionDate(
+                oneMonthFromToday.getDayOfMonth(), 
+                oneMonthFromToday.getMonthValue(),
+                oneMonthFromToday.getYear());
+        
+        dateWithOneLessThanCapacityAuctions = new AuctionDate(
+                oneMonthFromToday.getDayOfMonth(),
+                oneMonthFromToday.getMonthValue(),
+                oneMonthFromToday.getYear());
+        for (int i = 0; i < AuctionDate.MAX_AUCTIONS - 1; i++)
+            dateWithOneLessThanCapacityAuctions.addAuction(new Auction(LocalDate.now().plusDays(30), 10, 4));
+        
+        dateWithCapacityAuctions = new AuctionDate(
+                oneMonthFromToday.getDayOfMonth(),
+                oneMonthFromToday.getMonthValue(),
+                oneMonthFromToday.getYear());
+        for (int i = 0; i < AuctionDate.MAX_AUCTIONS; i++)
+            dateWithCapacityAuctions.addAuction(new Auction(LocalDate.now().plusDays(30), 10, 4));
+    }
 
     /**
-     * Tests the addAuction method. Specifically, determines that an auction was added when no
+     * Determines that an auction was added when no
      * auctions are scheduled yet on a date.
      */
     @Test
     public void addAuction_noAuctionsOnRequestedDate_newAuctionAdded() {
-        AuctionDate dateTest = new AuctionDate(30, 5, 2018);
-        Auction auction1 = new Auction();
-        assertFalse("Should not be at capacity", dateTest.isAtCapacity());
-        dateTest.addAuction(auction1);
-        assertSame("date should hold this auction now", auction1, dateTest.getAuctions().get(0));
+        assertEquals("date should hold no auctions yet", 0, 
+                dateWithNoAuctions.getAuctions().size());
+        dateWithNoAuctions.addAuction(new Auction(LocalDate.now().plusDays(30), 10, 4));
+        assertEquals("date should hold one auction now", 1, 
+                dateWithNoAuctions.getAuctions().size());
     }
 
     /**
-     * Tests the addAuction method. Specifically, determines that an auction was added 
-     * when less than the capacity number of auctions are scheduled for a date.
+     * Determines that an auction was added when less than the 
+     * capacity number of auctions are scheduled for a date.
      */
     @Test
     public void addAuction_lessThanMaxAuctionsOnRequestedDate_newAuctionAdded() {
-        AuctionDate dateTest = new AuctionDate(30, 5, 2018);
-        Auction auction1 = new Auction();
-        Auction auction2 = new Auction();
-        dateTest.addAuction(auction1);
-        assertFalse("Should not be at capacity", dateTest.isAtCapacity());
-        dateTest.addAuction(auction2);
-        assertSame("date should hold this auction now", auction1, dateTest.getAuctions().get(0));
-        assertSame("date should hold this auction now", auction2, dateTest.getAuctions().get(1));
+        dateWithOneLessThanCapacityAuctions.addAuction(new Auction(LocalDate.now().plusDays(30), 10, 4));
+        assertEquals("date should hold capacity auctions now", 
+                AuctionDate.MAX_AUCTIONS, 
+                dateWithOneLessThanCapacityAuctions.getAuctions().size());
+        assertTrue("Should be at capacity", 
+                dateWithOneLessThanCapacityAuctions.isAtCapacity());
     }
 
     /**
-     * Tests the addAuction method. Specifically, checks that an exception is thrown when 
-     * attempting to schedule an auction when a date is at capacity.
+     * Checks that an exception is thrown when attempting to 
+     * schedule an auction when a date is at capacity.
      */
     @Test (expected = IllegalArgumentException.class)
     public void addAuction_maxAuctionsOnRequestedDate_ExceptionThrown() {
-        AuctionDate dateTest = new AuctionDate(30, 5, 2018);
-        dateTest.addAuction(new Auction());
-        dateTest.addAuction(new Auction());
-        dateTest.addAuction(new Auction());
+        dateWithCapacityAuctions.addAuction(new Auction(LocalDate.now().plusDays(30), 10, 4));
     }
 
     /**
-     * Tests the isAtCapacity method. Specifically, checks that isAtCapacity returns the 
+     * Checks that isAtCapacity returns the 
      * correct value when it is and is not at capacity.
      */
     @Test
     public void isAtCapacity_maxAuctionsOnRequestedDate_shouldBeTrue() {
-        AuctionDate dateTest = new AuctionDate(30, 5, 2018);
-        for (int i = 0; i < AuctionDate.MAX_AUCTIONS; i++) {
-            assertFalse("Date should not yet be at capacity", dateTest.isAtCapacity());
-            dateTest.addAuction(new Auction());
-        }
-        assertTrue("Date should be at capacity", dateTest.isAtCapacity());
+        assertTrue("Date should be at capacity", 
+                dateWithCapacityAuctions.isAtCapacity());
     }
 }
---
+++ src/model/AuctionItemTest.java
@@ -0,0 +1,64 @@
+/*
+ * TCSS 360 - Software Development & Quality Techniques
+ * Group 1
+ * AuctionCentral
+ */
+package model;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+import java.math.BigDecimal;
+import java.util.LinkedList;
+
+import org.junit.Before;
+import org.junit.Test;
+
+
+
+
+/**
+ * Tests the AuctionItem class and its functionality.
+ * 
+ * @author Steven Golob 
+ * @version April 30, 2018
+ */
+public class AuctionItemTest {
+    
+    private AuctionItem testItem;
+    
+    private Bidder bidderMakingBidsOnTestItem;
+    
+    @Before
+    public void setup() {
+        testItem = new AuctionItem(BigDecimal.valueOf(35.0), new LinkedList<Bid>(), "BB-gun");
+        bidderMakingBidsOnTestItem = new Bidder("bidder1", "StevenTest");
+    }
+    
+    /**
+     * Test the isBidValid method. Specifically, checks that a bid is valid for the 
+     * amount equal to minimum bid amount .
+     */
+    @Test
+    public void isBidValid_equalToMinimumBid_shouldBeTrue() {
+        assertTrue("Equal bid to minimum amount is valid", testItem.isBidValid(bidderMakingBidsOnTestItem, BigDecimal.valueOf(35.0)));
+    }
+
+    /**
+     * Test the isBidValid method. Specifically, checks that a bid is valid for an amount 
+     * greater than the minimum bid amount.
+     */
+    @Test
+    public void isBidValid_greaterThanMinimumBid_shouldBeTrue() {
+        assertTrue("Equal bid to minimum amount is valid", testItem.isBidValid(bidderMakingBidsOnTestItem, BigDecimal.valueOf(35.01)));
+    }
+
+    /**
+     * Test the isBidValid method. Specifically, checks that a bid is not valid for an 
+     * amount less than the minimum bid amount.
+     */
+    @Test
+    public void isBidValid_lessThanMinimumBid_shouldBeFalse() {
+        assertFalse("Equal bid to minimum amount is valid", testItem.isBidValid(bidderMakingBidsOnTestItem, BigDecimal.valueOf(34.99)));
+    }
+}
