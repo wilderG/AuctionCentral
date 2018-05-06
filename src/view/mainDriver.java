1815d948dd277e356fba34417462e06e71bfd422
yohei-yohei <yohei03@uw.edu>
2018-05-05 18:58:44 -0700

    updated view classess

--- src/view/mainDriver.java
+++ src/view/mainDriver.java
@@ -7,16 +7,17 @@ import model.Bidder;
 
 public class mainDriver {
 
-
+	static Scanner input = new Scanner(System.in);
 	public static void main(String[] theArgs) {
 		auctionBidSearch test = new auctionBidSearch();
 		viewBids test2 = new viewBids();
-		Scanner input = new Scanner(System.in);
+
 		int option;
 		do {
-		System.out.println("1.Search for auctions to bid on");
-		System.out.println("2.view current bids");
-		System.out.println("3.logout");
+		System.out.println("MAIN MENU");
+		System.out.println("	1.Search for auctions to bid on");
+		System.out.println("	2.view current bids");
+		System.out.println("	3.logout");
 		System.out.print("Choice: ");
 		option = input.nextInt();
 		if (option == 1) {
