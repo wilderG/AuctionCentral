2d49a154c558cbddbfaa12b3f6a155184df59cb2
Wilder Garcia <wgarcia2@uw.edu>
2018-05-05 20:01:11 -0700

    updates classes

---
+++ src/view/viewNonProfit.java
@@ -0,0 +1,33 @@
+package view;
+
+import java.util.Scanner;
+
+public class viewNonProfit extends mainDriver {
+	static Scanner input = new Scanner(System.in);
+	public void disPlay() {
+		auctionBidSearch test = new auctionBidSearch();
+		viewBids test2 = new viewBids();
+		int option;
+		System.out.println("MAIN MENU");
+		System.out.println("	1.View all auctions/items for Salvation Army");
+		System.out.println("	2.Submit an auction request");
+		System.out.println("	3.Modify auction inventory");
+		System.out.println("	4.logout");
+		System.out.print("Choice: ");
+		option = input.nextInt();
+		do {
+		if (option == 1) {
+		
+		} 
+		if (option ==2) {
+			
+		} 
+		if(option ==3) {
+		}
+		if(option ==4) {
+		}
+		} while(option!=4);
+		System.out.println("Sesion has end");
+	}
+	}
+
