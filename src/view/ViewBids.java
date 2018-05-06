0a3fbec9d45595f68df4f1e132975360dc11dada
Jared Malone <jaredmm@uw.edu>
2018-05-06 16:59:02 -0700

    updated ViewBids choice

--- src/view/ViewBids.java
+++ src/view/ViewBids.java
@@ -5,14 +5,10 @@ import java.util.Scanner;
 public class ViewBids {
 
 	public int viewOptions(Scanner theScanner) {
-	    int choice;
-		System.out.println("Options:");
+	    System.out.println("Options:");
         System.out.println("  0. To go back to the previous screen");
 		System.out.println("  1. View all auctions in which I have placed bids");
 		System.out.println("  2. View all items that I have placed bids on");
-		do {
-		    choice = mainDriver.getNextInt();
-		} while (choice > 2);
-		return choice;
+		return mainDriver.getNextInt(2);
 	}
 }
