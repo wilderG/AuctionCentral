21c36b0d252ddbecacdb8d27ad66500b72d40598
yohei-yohei <yohei03@uw.edu>
2018-05-06 20:45:33 -0700

    update

--- src/view/ViewBids.java
+++ src/view/ViewBids.java
@@ -3,11 +3,13 @@ package view;
 import java.util.Scanner;
 
 public class ViewBids {
-
+	/*
+	 * 
+	 */
 	public int viewOptions(Scanner theScanner) {
-
 		System.out.println("Options:");
-		System.out.println("  1. View all auctions in which I have placed bids");
+		System.out.println("  1. View all auctions in which"
+				+ " I have placed bids");
 		System.out.println("  2. View all items that I have placed bids on");
 		System.out.println("To go back to the previous screen enter 0");
 		System.out.print("Choice: ");
