f4d59160429af950a31414da73d30a4f4ea1f3bb
Jared Malone <jaredmm@uw.edu>
2018-05-06 17:00:56 -0700

    changed viewBids getNextInt

--- src/view/ViewBids.java
+++ src/view/ViewBids.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 public class ViewBids {
 
 	public int viewOptions(Scanner theScanner) {
-	    System.out.println("Options:");
+	    final int maxOption = 2;
+		System.out.println("Options:");
         System.out.println("  0. To go back to the previous screen");
 		System.out.println("  1. View all auctions in which I have placed bids");
 		System.out.println("  2. View all items that I have placed bids on");
-		return mainDriver.getNextInt(2);
+		return mainDriver.getNextInt(maxOption);
 	}
 }
