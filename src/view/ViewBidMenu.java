7c7471df4965908da4f74c3fea93ea8128963c2c
jim-rosales <rosales.jim@outlook.com>
2018-05-06 18:35:31 -0700

    Add fix for processing a new bid

--- src/view/ViewBidMenu.java
+++ src/view/ViewBidMenu.java
@@ -40,6 +40,7 @@ public class ViewBidMenu {
 		NewBidRequest theRequest = new NewBidRequest(theUser, theAuction, theItem, bidAmount);
 		Bid userBid = theManager.processNewBid(theRequest);
 		theAuction.addBid(theUser, userBid);
+		theUser.addAuction(theAuction);
 		System.out.println("Bid Successfully placed!");
 		System.out.println("Press any key to continue");
 		theScanner.next();
