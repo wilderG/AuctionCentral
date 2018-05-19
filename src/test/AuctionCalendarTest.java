a25dea35ea71f9133f64dd8129645ae3e4cc7f0a
Steven Golob <golobs@uw.edu>
2018-05-19 16:47:00 -0700

    Added test methods for new user stories 1, 2, 3

--- src/test/AuctionCalendarTest.java
+++ src/test/AuctionCalendarTest.java
@@ -6,6 +6,9 @@ package test;
 import static org.junit.Assert.*;
 
 import java.time.LocalDate;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.LinkedList;
 
 import org.junit.Before;
 import org.junit.Rule;
@@ -166,5 +169,142 @@ public class AuctionCalendarTest {
 		assertFalse("The date less than the minumum days out from the current date is acceptable", 
 				theCalendar.isDateWithinEligableRange(theNonAcceptableDateBeforeMinimumDaysOutAndWithinMaximumDaysOut));
 	}
+	
+	@Test (expected = IllegalArgumentException.class)
+	public void setMaxUpcomingAuction_zeroGiven_exceptionThrown() {
+	    theCalendar.setMaximumUpcomingAuctions(0);
+	}
+	
+    @Test (expected = IllegalArgumentException.class)
+    public void setMaxUpcomingAuction_negativeGiven_exceptionThrown() {
+        theCalendar.setMaximumUpcomingAuctions(-1);
+    }
 
+    @Test
+    public void 
+    setMaxUpcomingAuction_numberGreaterThanExistingFutureAuctions_passes() {
+        for (int i = 0; i < theCalendar.getMaximumUpcomingAuctions(); i++) {
+            LocalDate testDate = LocalDate.now().plusDays(i + AuctionCalendar.MINIMUM_DAYS_OUT);
+            theCalendar.submitAuction(new Auction(null, auctionOwner), 
+                    testDate.getDayOfMonth(), testDate.getMonthValue(), 
+                    testDate.getYear());
+        }
+        assertFalse(theCalendar.isAllowingNewAuction());
+        assertEquals(theCalendar.getMaximumUpcomingAuctions(), AuctionCalendar.DEFAULT_MAXIMUM_FUTURE_AUCTIONS);
+        
+        theCalendar.setMaximumUpcomingAuctions(theCalendar.getMaximumUpcomingAuctions() + 1);
+        assertTrue(theCalendar.isAllowingNewAuction());
+        assertEquals(theCalendar.getMaximumUpcomingAuctions(), AuctionCalendar.DEFAULT_MAXIMUM_FUTURE_AUCTIONS + 1);
+    }
+    
+    @Test
+    public void setMaxUpcomingAuction_numberGivenLessThanCurrentCapacity_passes() {
+        for (int i = 0; i < theCalendar.getMaximumUpcomingAuctions(); i++) {
+            LocalDate testDate = LocalDate.now().plusDays(i + AuctionCalendar.MINIMUM_DAYS_OUT);
+            theCalendar.submitAuction(new Auction(null, auctionOwner), 
+                    testDate.getDayOfMonth(), testDate.getMonthValue(), 
+                    testDate.getYear());
+        }
+        assertFalse(theCalendar.isAllowingNewAuction());
+        theCalendar.setMaximumUpcomingAuctions(theCalendar.getMaximumUpcomingAuctions() - 10);
+        assertFalse(theCalendar.isAllowingNewAuction());
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void getAuctionsWithinRange_secondDateBeforeFirst_exceptionThrown() {
+        theCalendar.getAuctionsWithinRange(LocalDate.now(), LocalDate.now().minusDays(1));
+    }
+
+    @Test
+    public void getAuctionsWithinRange_secondDateEqualsFirst_getsAuctions() {
+        Auction testAuction1 = new Auction(null, auctionOwner);
+        Auction testAuction2 = new Auction(null, auctionOwner);
+        Auction testAuction3 = new Auction(null, auctionOwner);
+        LocalDate testDate1 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT);
+        LocalDate testDate2 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT + 1);
+        LocalDate testDate3 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT + 2);
+        theCalendar.submitAuction(testAuction1, testDate1.getDayOfMonth(), testDate1.getMonthValue(), testDate1.getYear());
+        theCalendar.submitAuction(testAuction2, testDate2.getDayOfMonth(), testDate2.getMonthValue(), testDate2.getYear());
+        theCalendar.submitAuction(testAuction3, testDate3.getDayOfMonth(), testDate3.getMonthValue(), testDate3.getYear());
+        
+        Collection<Auction> auctionsInRange = theCalendar.getAuctionsWithinRange(testDate2, testDate2);
+        assertEquals(1, auctionsInRange.size());
+        assertEquals(testAuction2, auctionsInRange.iterator().next());
+    }
+    
+    @Test
+    public void getAuctionsWithinRange_secondDateAfterFirst_getsAuctions() {
+        Auction testAuction1 = new Auction(null, auctionOwner);
+        Auction testAuction2 = new Auction(null, auctionOwner);
+        Auction testAuction3 = new Auction(null, auctionOwner);
+        Auction testAuction4 = new Auction(null, auctionOwner);
+        Auction testAuction5 = new Auction(null, auctionOwner);
+        LocalDate testDate1 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT);
+        LocalDate testDate2 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT + 1);
+        LocalDate testDate3 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT + 2);
+        LocalDate testDate4 = LocalDate.now().plusDays(AuctionCalendar.MINIMUM_DAYS_OUT + 3);
+        theCalendar.submitAuction(testAuction1, testDate1.getDayOfMonth(), testDate1.getMonthValue(), testDate1.getYear());
+        theCalendar.submitAuction(testAuction2, testDate2.getDayOfMonth(), testDate2.getMonthValue(), testDate2.getYear());
+        theCalendar.submitAuction(testAuction3, testDate3.getDayOfMonth(), testDate3.getMonthValue(), testDate3.getYear());
+        theCalendar.submitAuction(testAuction4, testDate3.getDayOfMonth(), testDate3.getMonthValue(), testDate3.getYear());
+        theCalendar.submitAuction(testAuction5, testDate4.getDayOfMonth(), testDate4.getMonthValue(), testDate4.getYear());
+        Collection<Auction> expectedAuctionsInRange = new LinkedList<>();
+        expectedAuctionsInRange.add(testAuction2);
+        expectedAuctionsInRange.add(testAuction3);
+        expectedAuctionsInRange.add(testAuction4);
+        
+        Collection<Auction> auctionsInRange = theCalendar.getAuctionsWithinRange(testDate2, testDate3);
+        assertEquals(expectedAuctionsInRange.size(), auctionsInRange.size());
+        assertTrue(auctionsInRange.containsAll(expectedAuctionsInRange));
+    }
+
+    @Test
+    public void getAuctionsWithinRange_rangeContainsPastActions_getsAuctions() {
+        LocalDate testDate1 = LocalDate.now().minusDays(20);
+        LocalDate testDate2 = LocalDate.now().minusDays(19);
+        LocalDate testDate3 = LocalDate.now().minusDays(18);
+        LocalDate testDate4 = LocalDate.now().minusDays(17);
+        Auction testAuction1 = new Auction(testDate1, auctionOwner);
+        Auction testAuction2 = new Auction(testDate2, auctionOwner);
+        Auction testAuction3 = new Auction(testDate2, auctionOwner);
+        Auction testAuction4 = new Auction(testDate3, auctionOwner);
+        Auction testAuction5 = new Auction(testDate4, auctionOwner);
+        theCalendar.forceAddAuctionInThePast(testAuction1);
+        theCalendar.forceAddAuctionInThePast(testAuction2);
+        theCalendar.forceAddAuctionInThePast(testAuction3);
+        theCalendar.forceAddAuctionInThePast(testAuction4);
+        theCalendar.forceAddAuctionInThePast(testAuction5);
+        Collection<Auction> expectedAuctionsInRange = new LinkedList<>();
+        expectedAuctionsInRange.add(testAuction2);
+        expectedAuctionsInRange.add(testAuction3);
+        expectedAuctionsInRange.add(testAuction4);
+        
+        Collection<Auction> auctionsInRange = theCalendar.getAuctionsWithinRange(testDate2, testDate3);
+        assertEquals(expectedAuctionsInRange.size(), auctionsInRange.size());
+        assertTrue(auctionsInRange.containsAll(expectedAuctionsInRange));
+    }
+    
+    @Test
+    public void 
+    getAllAuctionsSorted_hasMultipleAuctionsWithDifferentDatesAddedInUnsortedOrder_returnsAllSorted() {
+        for (int i = 1; i <= 5; i++) {
+            theCalendar.forceAddAuctionInThePast(new Auction(LocalDate.now().minusDays(i), auctionOwner));
+            LocalDate futureDate = LocalDate.now().plusDays(20 - i);
+            theCalendar.submitAuction(new Auction(futureDate, auctionOwner), 
+                    futureDate.getDayOfMonth(), futureDate.getMonthValue(), futureDate.getYear());
+        }
+        theCalendar.forceAddAuctionInThePast(new Auction(LocalDate.now(), auctionOwner));
+        
+        Collection<Auction> allAuctionsSorted = theCalendar.geAllAuctionsSorted();
+        assertEquals(11, allAuctionsSorted.size());
+        
+        Iterator<Auction> itr = allAuctionsSorted.iterator();
+        Auction prev = itr.next();
+        Auction next;
+        while (!itr.hasNext()) {
+            next = itr.next();
+            assertTrue(next.getDate().isAfter(prev.getDate()));
+            prev = next;
+        }
+    }
 }
