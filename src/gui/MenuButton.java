538a4dcf315723c1d98aacb0a3cbccd0914ead6d
jim-rosales <rosales.jim@outlook.com>
2018-05-21 18:01:03 -0700

    Add scrollPane to be used by the informationContainer

---
+++ src/gui/MenuButton.java
@@ -0,0 +1,28 @@
+package gui;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+
+/**
+ * This class generates new menu buttons for use in the menuBar
+ * @author Jared Malone
+ *
+ */
+public class MenuButton {
+	public static AnchorPane newMenuButton(String theTitle) {
+		FXMLLoader buttonLoader = new FXMLLoader(SessionController.class.getResource("ViewMenuButton.fxml"));
+		AnchorPane button = null;
+		try {
+			button = (AnchorPane) buttonLoader.load();
+		} catch (IOException e) {
+			System.err.println("Button Error");
+			//e.printStackTrace();
+		}	
+		
+		ViewMenuButtonController buttonCtrl = (ViewMenuButtonController) buttonLoader.getController();
+		buttonCtrl.setText(theTitle);
+		return button;
+	}
+}
